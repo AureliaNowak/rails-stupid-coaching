f15a51b25e45fac5e05a5a60d4e4760e6b49506b
Aurelia Nowak <nowakaurelia@gmail.com>
2020-02-17 15:18:08 +0100

    Rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+    @user_input = params[:user_input]
+  end
+
+  def answer
+    @your_message = params[:question]
+
+    if @your_message == 'i am going to work right now!'
+      @answer = 'Great!'
+    elsif @your_message.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
